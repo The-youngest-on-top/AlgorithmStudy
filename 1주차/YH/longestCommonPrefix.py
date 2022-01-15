a8f28cb9352926857f951783e525dd1331280ede
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-01-15 17:55:47 +0900

    longestCommonPrefix

--- 1주차/YH/longestCommonPrefix.py
+++ 1주차/YH/longestCommonPrefix.py
@@ -11,6 +11,5 @@ class Solution:
             for j in range(1, len(strs_list)) :
                 if strs_list[0][i] != strs_list[j][i] : 
                     return strs[0][:i]
-                
         
         return answer
