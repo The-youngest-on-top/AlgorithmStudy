7ea5bacec4095e88df8ae33ed900fce50c85ea56
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-01-15 17:49:54 +0900

    twoSum

---
+++ 1주차/YH/longestCommonPrefix.py
@@ -0,0 +1,15 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        answer = ''
+        strs_list = []
+        
+        for i in range(len(strs)) : 
+            strs_list.append(strs[i])
+            strs_list.sort(key=len)
+            
+        for i in range(len(strs_list[0])) :             
+            for j in range(1, len(strs_list)) :
+                if strs_list[0][i] != strs_list[j][i] : 
+                    return strs[0][:i]
+        
+        return answer
--- 1주차/YH/twoSum.py
+++ 1주차/YH/twoSum.py
@@ -1,3 +1,7 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        
+        for i in range(len(nums)) : 
+            j = i + 1
+            for j in range(i) : 
+                if (nums[i] + nums[j] == target) : 
+                    return [i, j]
