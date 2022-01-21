a1c2c6ff7d1ac93cd5dfe0d7be3c967ddb821d51
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-01-21 11:57:28 +0900

    mergeTwoLists

--- 1주차/YH/mergeTwoLists.py
+++ 1주차/YH/mergeTwoLists.py
@@ -3,7 +3,6 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         answer = ListNode(0)
