492eb2e7711460d5264501e5505d135990fc0de7
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-01-22 23:05:29 +0900

    YH

--- 1주차/YH/strStr.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def strStr(self, haystack: str, needle: str) -> int:
-        
-        answer = -1
-       
-        if needle in haystack:
-            answer = haystack.index(needle)
-        
-        return answer
