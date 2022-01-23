b8075879353f000eb96e4e6d227e54b6da2f895c
Kim Kang Min <41673190+kkmin223@users.noreply.github.com>
2022-01-23 16:44:52 +0900

    Update 53-maximum-subarray.java

--- 2주차/KM/53-maximum-subarray.java
+++ 2주차/KM/53-maximum-subarray.java
@@ -1,3 +1,4 @@
+//방법1
 class Solution {
     public int maxSubArray(int[] nums) {
         int[] dp = nums; // dp = 해당 인덱스에서 가장 큰 값을 저장하는 배열
@@ -10,6 +11,19 @@ class Solution {
             }
             if(max < dp[i])
                     max = dp[i];
+        }
+        return max;
+    }
+}
+
+//방법2
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int max = nums[0]; // 최대값 저장
+        int sum = nums[0]; // 지금까지 sum 값
+        for(int i = 1; i<nums.length; i++){
+            sum = Math.max(sum+nums[i] , nums[i]); //sum + 현재값 과 현재값 중 큰것을 sum에 저장
+            max = Math.max(sum,max); // sum이 큰 지 max가 큰 지 비교
         }
         return max;
     }
