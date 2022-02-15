16dc310be07913630c3e69f72181c674c63f3e0c
knh6269 <knh6269@naver.com>
2022-02-15 15:01:09 +0900

    merge

--- 2주차/Plus One.js
+++ 2주차/Plus One.js
@@ -1,17 +1,17 @@
 function plusOne(digits) {
+
     let number="";
     let answer=[];
+    //digits 배열의 배열값들을 문자열에 더해준다.
     for(let num of digits){
         number+=num;
     }
-   console.log(typeof(number));
+    //덧셈을 위해 number를 BigInt형으로 변환 후 1을 더해준다.
    number=BigInt(number)+BigInt(1);
-   console.log(typeof(number));
-
+   //배열로 변환을 위해 number를 문자열로 변환해준다.
    number=String(number);
-       console.log(typeof(number));
-
-  
+   
+   //문자열을 answer 배열에 푸쉬
    for(let num of number){
       answer.push(num);
    }
