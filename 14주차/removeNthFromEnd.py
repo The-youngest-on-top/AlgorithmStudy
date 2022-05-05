088f18496761ac4e3ff5f99d05bfa30ade59d7c3
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-05-06 01:24:42 +0900

    14주차YH

---
+++ 14주차/removeNthFromEnd.py
@@ -0,0 +1,29 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        
+        if head.next == None : return None
+        
+        tmp = head
+        size = 0
+        
+        # size 구하기
+        while tmp :
+            size += 1
+            tmp = tmp.next
+            
+        tmp = head
+        
+        # size와 지워야 하는 위치가 같다면 첫 번째 값을 지우기
+        if n == size : return head.next
+        
+        # 지워야 하는 값 이전의 node들을 tmp에 저장
+        for i in range(size-n-1) : tmp = tmp.next
+            
+        # 지워야 하는 값 이전과 이후의 값들을 연결
+        tmp.next = tmp.next.next
+        return head 
