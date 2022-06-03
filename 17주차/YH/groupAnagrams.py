28ac67ef7198a60410c46f669f51a991a3360103
JungYeonHwi <64068511+JungYeonHwi@users.noreply.github.com>
2022-06-04 00:37:37 +0900

    17주차 YH
    17주차 YH

---
+++ 17주차/YH/groupAnagrams.py
@@ -0,0 +1,11 @@
+import collections
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+        anagrams = collections.defaultdict(list) # 디폴트값을 list로 줌
+
+        for word in strs : # 정렬하여 딕셔너리에 추가
+            anagrams[''.join(sorted(word))].append(word)
+
+        return list(anagrams.values())
