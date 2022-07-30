adf42e316a51540f33bf9d2ab4118e8f5d90da01
qor4 <qor_4@naver.com>
2022-07-30 23:09:41 +0900

    24주차 다은

---
+++ 24주차/DE/1137. N-th Tribonacci Number.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+ function tribonacci(n) {
+    let dp = new Array(n+1)
+    dp[0] = 0
+    dp[1] = 1
+    dp[2] = 1
+    for(let i = 3; i <=n; i++){
+        dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+    }
+    return dp[n]
+    
+};
