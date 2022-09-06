d5088acef7b1530df2685a76bad0407066097cb7
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-09-06 22:08:42 +0900

    29주차 수정

--- fairCandySwap.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def fairCandySwap(self, aliceSizes: List[int], bobSizes: List[int]) -> List[int]:
-        
-        a, diff = set(aliceSizes), (sum(aliceSizes) - sum(bobSizes)) // 2
-        
-        for b in bobSizes :
-            if b + diff in a :
-                return [b + diff, b]
--- smallestRangeI.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def smallestRangeI(self, nums: List[int], k: int) -> int:
-        
-        l, r = min(nums) + k, max(nums) - k
-        
-        return 0 if l >= r else r - l
