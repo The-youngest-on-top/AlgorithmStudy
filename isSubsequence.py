7f3ad0af6af17aa5b1eb35d9a6d8e300735464d1
JungYeonHwi <64068511+JungYeonHwi@users.noreply.github.com>
2022-09-06 22:11:06 +0900

    29주차 수정
    29주차 수정

--- isSubsequence.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def isSubsequence(self, s: str, t: str) -> bool:
-        
-        t = iter(t)
-        
-        return all(c in t for c in s)
--- wordPattern.py
+++
@@ -1,5 +0,0 @@
-class Solution:
-    def wordPattern(self, pattern: str, s: str) -> bool:
-        
-        words = s.split(' ')
-        return len(pattern) == len(words) and len(set(pattern)) == len(set(words)) == len(set(zip(pattern, words)))
